620b4c31098016689959f5bc1ade994a8011e979
FaLk0-exe <87442745+FaLk0-exe@users.noreply.github.com>
2022-09-18 17:17:12 +0300

    RegexCollection

--- CryptoTask/Services/Parsers/OpenApiJsonParser.cs
+++ CryptoTask/Services/Parsers/OpenApiJsonParser.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CryptoTask.Services
+namespace CryptoTask.Services.Parsers
 {
     public static class OpenApiJsonParser
     {
---
+++ CryptoTask/Services/RegexCollections/HttpRequestRegexCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CryptoTask.Services.RegexCollections
+{
+    public static class HttpRequestRegexCollection
+    {
+        public static readonly Regex AssetsRequestRegex =
+        new Regex(@"^https://cryptingup\.com/api/assets");
+    }
+}

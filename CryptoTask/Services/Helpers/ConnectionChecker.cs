7589c6a4c3ee590f409e76d24553de34648ed00f
FaLk0-exe <87442745+FaLk0-exe@users.noreply.github.com>
2022-09-18 17:18:53 +0300

    Helpers

---
+++ CryptoTask/Services/Helpers/ConnectionChecker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTask.Services.Helpers
+{
+    public static class ConnectionChecker
+    {
+        public static bool OK()
+        {
+            try
+            {
+                Dns.GetHostEntry("dotnet.beget.tech");
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}

acb04ee66ae6aa743987a67dc22670d1c11435a7
FaLk0-exe <87442745+FaLk0-exe@users.noreply.github.com>
2022-09-18 21:01:23 +0300

    Model's refactoring

--- CryptoTask/Models/Asset.cs
+++ CryptoTask/Models/Asset.cs
@@ -10,20 +10,15 @@ namespace CryptoTask.Models
     [Serializable]
     public class Asset
     {
-        [JsonProperty("quote")]
-        public Quote quote { get; set; }
+
         [JsonProperty("asset_id")]
         public string assetId { get; set; }
         [JsonProperty("name")]
         public string name { get; set; }
-        [JsonProperty("description")]
-        public string description { get; set; }
+
         [JsonProperty("website")]
         public string website { get; set; }
-        [JsonProperty("ethereum_contract_address")]
-        public string ethereumContractAddress { get; set; }
-        [JsonProperty("pegged")]
-        public string pegged { get; set; }
+
         [JsonProperty("price")]
         public decimal price { get; set; }
         [JsonProperty("volume_24h")]
@@ -34,22 +29,8 @@ namespace CryptoTask.Models
         public decimal change24h { get; set; }
         [JsonProperty("change_7d")]
         public decimal change7d { get; set; }
-        [JsonProperty("total_supply")]
-        public decimal totalSupply { get; set; }
-        [JsonProperty("circulating_supply")]
-        public decimal circulatingSupply { get; set; }
-        [JsonProperty("max_supply")]
-        public decimal maxSupply { get; set; }
-        [JsonProperty("market_cap")]
-        public decimal marketCap { get; set; }
-        [JsonProperty("fully_diluted_market_cap")]
-        public decimal fullyDilutedMarketCap { get; set; }
         [JsonProperty("status")]
-        public string status { get; set; }
-        [JsonProperty("created_at")]
-        public DateTime createdAt { get; set; }
-        [JsonProperty("updated_at")]
-        public DateTime updatedAt { get; set; }
+
 
     }
 }

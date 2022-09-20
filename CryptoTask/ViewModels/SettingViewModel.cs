7efaa307d10b239fb0e5c2f9d83e3c471d8b9a99
FaLk0-exe <87442745+FaLk0-exe@users.noreply.github.com>
2022-09-20 13:59:58 +0300

    SettingsViewModel

---
+++ CryptoTask/ViewModels/SettingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CryptoTask.Models;
+using CryptoTask.Services.Helpers;
+using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CryptoTask.ViewModels
+{
+    public class SettingViewModel:BaseVM
+    {
+        public ICommand ChangeTheme
+        {
+            get
+            {
+                return new DelegateCommand<string>(
+                    (theme) =>
+                    {
+                        if (theme == "Dark")
+                            ThemeSwitcher.SwitchDarkTheme();
+                        if(theme=="Light")
+                            ThemeSwitcher.SwitchDarkTheme();
+                    });
+            }
+        }
+    }
+}

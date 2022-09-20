5aab91316a8683be85950e35b71f1830b5b02fb0
FaLk0-exe <87442745+FaLk0-exe@users.noreply.github.com>
2022-09-20 12:48:09 +0300

    ThemeSwitcher

---
+++ CryptoTask/Services/Helpers/ThemeSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CryptoTask.Services.Helpers
+{
+    public static class ThemeSwitcher
+    {
+        private static void SwitchTheme(string uriStr)
+        {
+            var uri = new Uri(uriStr, UriKind.Relative);
+            ResourceDictionary rd = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(rd);
+        }
+
+        public static void SwitchLightTheme()
+        {
+            SwitchTheme("Dictionaries/LightDictionary.xaml");
+        }
+
+        public static void SwitchDarkTheme()
+        {
+            SwitchTheme("Dictionaries/DarkDictionary.xaml");
+        }
+    }
+}
